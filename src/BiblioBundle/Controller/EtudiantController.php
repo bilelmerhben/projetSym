22c94697b89820b7723d5c67bb40795d3a09afe4
bilelmerhben <53703326+bilelmerhben@users.noreply.github.com>
2020-12-25 21:22:30 +0100

    firt

--- src/BiblioBundle/Controller/EtudiantController.php
+++ src/BiblioBundle/Controller/EtudiantController.php
@@ -6,7 +6,9 @@ namespace BiblioBundle\Controller;
 use BiblioBundle\Entity\Etudiant;
 use BiblioBundle\Form\EtudiantForm;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 
 class EtudiantController extends Controller
@@ -30,18 +32,66 @@ class EtudiantController extends Controller
 
     public function listAction()
     {
-        $etudiants = array ("hamza","salah","zied");
-        return $this->render('@Biblio/Etudiant/list.html.twig',array('etudiants'=>$etudiants ));
+        $em = $this->container->get('doctrine') ->getEntityManager();
+        $etudiants = $em->getRepository('BiblioBundle:Etudiant')->findAll();
+        return $this->render('@Biblio/Etudiant/list.html.twig',array(
+            'etudiants'=>$etudiants
+        ));
     }
 
-    public function addAction()
+    public function addAction(Request $request)
     {
         $Etudiant = new Etudiant();
         $form = $this->createForm(EtudiantForm::class,$Etudiant);
+       $form->handleRequest($request);
+       if($form->isSubmitted() && $form->isValid())
+       {
+           $em= $this->getDoctrine()->getManager();
+           $em->persist($Etudiant);
+           $em->flush();
+           return $this->redirect($this->generateUrl("list_etudiant"));
+
+       }
         return $this->render('@Biblio/Etudiant/add.html.twig',
             array(
                 'Form'=>$form->createView()
             ));
 
+    }
+
+    public function deleteAction(Request $request, $id)
+    {
+        $em= $this->getDoctrine()->getManager();
+        $etudiant = $em->getRepository('BiblioBundle:Etudiant')->find($id);
+        if($etudiant !== null)
+        {
+            $em->remove($etudiant);
+            $em->flush();
+        }
+        else
+        {
+            throw new NotFoundHttpException("L'étudient d'id".$id."n'existe pas");
+        }
+        return $this->redirectToRoute("list_etudiant");
+    }
+
+    public function updateAction(Request $request,$id)
+    {
+        $em= $this->getDoctrine()->getManager();
+        $Etudiant=$em->getRepository('BiblioBundle:Etudiant')->find($id);
+
+        $editform = $this->createForm(EtudiantForm::class ,$Etudiant);
+        $editform ->handleRequest($request);
+
+        if($editform->isSubmitted() && $editform->isValid())
+        {
+            $em->persist($Etudiant);
+            $em->flush();
+            return $this->redirect($this->generateUrl("list_etudiant"));
+        }
+        return $this->render('@Biblio/Etudiant/update.html.twig',
+            array(
+                'editForm'=>$editform->createView()
+            ));
     }
 }
